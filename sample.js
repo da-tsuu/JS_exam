f8734c30d7dc8b6f9027ca56defdb8ec0b0ecf66
takeru tsuda <da_tsuudayo@yahoo.co.jp>
2019-09-11 01:20:45 +0000

    JS課題を提出

---
+++ sample.js
@@ -0,0 +1,85 @@
+$(document).ready(function(){
+  function score_indicate(){
+    // このような記述をすることで、subject_pointsという変数の中に
+    // [国語の点数,英語の点数,数学の点数,理科の点数,社会の点数]という配列を作成できる。
+    let subject_points = [Number($('#national_language').val()),
+                          Number($('#english').val()),
+                          Number($('#mathematics').val()),
+                          Number($('#science').val()),
+                          Number($('#society').val())
+                          ];
+
+    // さらにこのような記述をすることで、「合計点：」となっている右の部分に合計点が出力される
+    let sum = 0;
+    for(let i=0; i<5; i++){
+      sum = sum + subject_points[i];
+      $("#sum_indicate").text(sum);
+    }
+    let avg = sum / 5
+    $("#average_indicate").text(avg);
+  };
+
+  function get_achievement(){
+    let evaluation;
+    let average = $('#average_indicate').text();
+    Number(average);
+    if(average >= 80){
+      evaluation = "A"
+    }else if(average >= 60){
+      evaluation = "B"
+    }else if(average >= 40){
+      evaluation = "C"
+    }
+    else{
+      evaluation = "D"
+    }
+    $("#evaluation").text(evaluation);
+    return evaluation;
+  }
+
+
+
+  function get_pass_or_failure(subject_points){
+    let judge;
+    let subject_point = [Number($('#national_language').val()),
+                          Number($('#english').val()),
+                          Number($('#mathematics').val()),
+                          Number($('#science').val()),
+                          Number($('#society').val())
+                          ];
+    for(let i=0; i<5; i++){
+      if(subject_point[i] >= 60){
+        judge = "合格"
+      }else{
+        judge = "不合格"
+      }
+    }
+      $("#judge").text(judge);
+      return judge;
+}
+
+
+
+  function judgement(){
+    // ここに、「最終ジャッジ」のボタンを押したら「あなたの成績はAです。合格です」といった内容を出力する処理を書き込む
+    // 下記の記述をすることで、「最終ジャッジ」のボタンを押すと「あなたの成績は（ここに「ランク」の値を入れる）です。（ここに「判定」の値を入れる）です」という文字の入った水色のフキダシが出力される処理が実装される。
+    let achievement = get_achievement()
+    let pass_or_failure = get_pass_or_failure()
+    $('#declaration').append(`<label id="alert-indicate" class="alert alert-info">あなたの成績は${achievement}です。${pass_or_failure}です</label>`);
+  };
+    $('#national_language, #english, #mathematics, #science, #society').change(function() {
+      score_indicate();
+    });
+
+    $('#btn-evaluation').click(function() {
+      get_achievement();
+    });
+
+    $('#btn-judge').click(function() {
+      get_pass_or_failure();
+    });
+
+    $('#btn-declaration').click(function() {
+      judgement();
+    });
+  });
