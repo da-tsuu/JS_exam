d932ea1c04063ed15af12eb7c953d76c78f3e572
takeru tsuda <da_tsuudayo@yahoo.co.jp>
2019-09-16 07:49:29 +0000

    課題を再々提出

--- sample.js
+++ sample.js
@@ -68,6 +68,7 @@ $(document).ready(function(){
     // 下記の記述をすることで、「最終ジャッジ」のボタンを押すと「あなたの成績は（ここに「ランク」の値を入れる）です。（ここに「判定」の値を入れる）です」という文字の入った水色のフキダシが出力される処理が実装される。
     let achievement = get_achievement()
     let pass_or_failure = get_pass_or_failure()
+    if (document.getElementById("alert-indicate") != null){ $('#alert-indicate').remove(); }
     $('#declaration').append(`<label id="alert-indicate" class="alert alert-info">あなたの成績は${achievement}です。${pass_or_failure}です</label>`);
   };
     $('#national_language, #english, #mathematics, #science, #society').change(function() {
